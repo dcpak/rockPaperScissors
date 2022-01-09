9c586135950cc9730684645d187cd5e212048a0c
Przemysław Kasprzak <przemyslaw.kasprzak@gmail.com>
2022-01-09 12:36:24 +0100

    Initial commit

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by przem on 09.01.2022.
+//
+
+#include "Game.h"
---
+++ Game.h
@@ -0,0 +1,14 @@
+//
+// Created by przem on 09.01.2022.
+//
+
+#ifndef ROCKPAPERSCISSORS_GAME_H
+#define ROCKPAPERSCISSORS_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //ROCKPAPERSCISSORS_GAME_H
